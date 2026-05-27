afa9496379b35c8d31fc5c790204a3ca8f5db18d
suriya9605 <suriyaj9605@gmail.com>
2026-05-27 10:19:44 +0530

    changes in server

--- nodejs/server/src/server.js
+++ nodejs/server/src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+export const _filename = fileURLToPath(import.meta.url);
+export const _dirname = path.dirname(_filename);
+
+console.log(_filename, "_filename");
+console.log(_dirname, "_dirname");
 
 app.get("/", (req, res) => {
   res.send("Hello from ES6 module!");
